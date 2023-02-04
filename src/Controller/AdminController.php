bed9da5913d0d5beafa35946de9c44b6cfec8d06
alexis fritsch <arckanics.reiko@gmail.com>
2023-02-04 10:41:30 +0100

    [FRONT] making admin react entry point in Twig

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -4,17 +4,17 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 #[Route('/mini-admin', name: 'app_admin_base')]
 class AdminController extends AbstractController
 {
     #[Route('/', name: 'app_admin')]
-    public function index(): JsonResponse
+    public function index(): Response
     {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/AdminController.php',
-        ]);
+      return $this->render('admin/index.html.twig', [
+        'controller_name' => 'AdminController',
+      ]);
     }
 }
