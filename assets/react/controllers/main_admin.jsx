dc18f95cb2eee9c9e4d73bad3489fbae7261e1f2
alexis fritsch <arckanics.reiko@gmail.com>
2023-02-04 10:46:55 +0100

    [FRONT] admin hello world

---
+++ assets/react/controllers/main_admin.jsx
@@ -0,0 +1,11 @@
+import React, { Component, Fragment } from "react"
+
+import Hello from '../components/helloWorld';
+
+// Index Root Component
+
+function MainAdmin() {
+  return <div><Hello/></div>
+}
+
+export default MainAdmin
