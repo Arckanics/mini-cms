78da29392a0d81a0da28f09e48678a7268603f2f
alexis fritsch <arckanics.reiko@gmail.com>
2023-02-06 14:59:42 +0100

    [FRONT] starting secure login component

---
+++ assets/react/components/ui/TxtInput.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function TxtInput({ id, name, value, label, placeholder }) {
+  return (
+    <div>
+      <label></label>
+      <input type="text" />
+    </div>
+  )
+}
+
+export default TxtInput
