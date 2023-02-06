0f08b0ea3f22e1f5f49056c8f2f57aa454365df9
alexis fritsch <arckanics.reiko@gmail.com>
2023-02-06 11:25:02 +0100

    [FRONT] navbar initial structure

--- src/Controller/AdminSecurityController.php
+++ src/Controller/AdminSecurityController.php
@@ -7,22 +7,18 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
-// #[Route('/mini-admin', name: 'app_admin_login')]
 class AdminSecurityController extends AbstractController
 {
     #[Route(path: '/mini-admin/login', name: 'app_login')]
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
-        // if ($this->getUser()) {
-        //     return $this->redirectToRoute('target_path');
-        // }
+        if ($this->getUser()) {
+          return $this->redirectToRoute('app_admin');
+        }
 
-        // get the login error if there is one
-        $error = $authenticationUtils->getLastAuthenticationError();
-        // last username entered by the user
-        $lastUsername = $authenticationUtils->getLastUsername();
-
-        return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
+        return $this->render('admin/index.html.twig', [
+          'url' => 'Login'
+        ]);
     }
 
     #[Route(path: '/mini-admin/logout', name: 'app_logout')]
