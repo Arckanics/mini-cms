ee0b4616a29e59705135e65f62312d2b6189ec7e
alexis fritsch <arckanics.reiko@gmail.com>
2023-02-07 16:28:08 +0100

    [BACK] dev profiler toolbar

--- config/bundles.php
+++ config/bundles.php
@@ -10,4 +10,5 @@ return [
     Symfony\UX\React\ReactBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
+    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
 ];
