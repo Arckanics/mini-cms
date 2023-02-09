e2a1c5cb136b3774bd70a0ecb4179829f366836e
alexis fritsch <arckanics.reiko@gmail.com>
2023-02-09 18:22:24 +0100

    [FRONT] optimizing & new css styles (login)

--- assets/react/components/ui/TxtInput.jsx
+++ assets/react/components/ui/TxtInput.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-function TxtInput({ id, value, label, placeholder, type, inputCls }) {
+function TxtInput({ id, value, label, placeholder, type, inputCls, divCls = 'input-wrap p-2', labelCls = 'block p-2' }) {
+  const [isActive, setActive] = useState(false)
+  const handleFocus = () => {
+    setActive(!isActive);
+  }
   return (
-    <div className='p-2'>
-      <label htmlFor={id} className='block p-2'>{label}</label>
-      <input type={type} id={id} className={inputCls} name={id} value={value} placeholder={placeholder} />
+    <div className={(divCls + (isActive ? " active" : ""))}>
+      <label htmlFor={id} className={labelCls}>{label}</label>
+      <input type={type} id={id} className={inputCls} name={id} value={value} onFocus={handleFocus} onBlur={handleFocus} placeholder={placeholder} />
     </div>
   )
 }
