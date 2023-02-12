ccadbfe296b4d28c52f1232744c48c759001c864
alexis fritsch <arckanics.reiko@gmail.com>
2023-02-12 14:54:48 +0100

    [FRONT] react router logic && login animation begin

--- assets/react/components/admin/pages/Login.jsx
+++ assets/react/components/admin/pages/Login.jsx
@@ -1,63 +1,56 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import Button from '../ui/Button'
 import TxtInput from '../ui/TxtInput'
 import { getToken } from '../Functions/Security'
 import { useNavigate } from 'react-router-dom'
 
-class Login extends Component {
-  constructor(props) {
-    super(props)
-    this.ajax = props.ajax
-    this.state = {
-      email: null,
-      password: null,
-      _token: getToken(),
-    }
+const Login = (props) => {
+  const nav = useNavigate()
+  const ajax = props.ajax
+  const [state, setState] = useState({
+    email: null,
+    password: null,
+    _token: getToken(),
+  })
 
-    this.handleChange = this.handleChange.bind(this)
-    this.handleSubmit = this.handleSubmit.bind(this)
+  const [isError, setIsError] = useState(false)
 
-  }
-
-  handleChange(e) {
-    this.setState(state => ({
+  const handleChange = (e) => {
+    setState(state => ({
       ...state,
       [e.target.name]: e.target.value
     }))
   }
 
-  handleSubmit(e) {
+  const handleSubmit = (e) => {
     e.preventDefault()
-    const {ajax} = this
-    this.state
-    ajax.post('/login', {...this.state})
+    ajax.post('/login', { ...state })
       .then(res => {
-        const nav = useNavigate()
-        nav('/mini-admin'+res.url)
+        nav('/mini-admin/')
+      }).catch(res => {
+        nav('/mini-admin/login')
       })
   }
 
-  render () {
-    const {email, password} = this.state
-    const {handleChange, handleSubmit} = this
-    return (
-      <form method="POST" id="login" className='rounded-lg bg-white color-dark' onSubmit={handleSubmit}>
-        <div className='form-title'>Connexion</div>
-        <div className='input-group-clear-outline'>
-          <TxtInput type="text" label="Compte(Email)" id="email" value={email} placeholder="adresse email..."
-            inputCls='input-txt' labelCls='label' onChange={handleChange}
-          />
-          <TxtInput type="password" label="Mot de passe" id="password" value={password} placeholder="mot de passe..."
-            inputCls='input-txt' labelCls='label' onChange={handleChange}
-          />
-        </div>
-        <Button
-          divCls='pt-1 p-4'
-          btnCls='btn-primary'
-        >Me Connecter</Button>
-      </form>
-    )
-  }
+  const { email, password } = state
+
+  return <form method="POST" id="login" className='rounded-lg bg-white color-dark' onSubmit={handleSubmit}>
+    <div className='form-title'>Connexion</div>
+    <div className='input-group-clear-outline'>
+      <TxtInput type="text" label="Compte(Email)" id="email" value={email} placeholder="adresse email..."
+        inputCls='input-txt' labelCls='label' onChange={handleChange}
+      />
+      <TxtInput type="password" label="Mot de passe" id="password" value={password} placeholder="mot de passe..."
+        inputCls='input-txt' labelCls='label' onChange={handleChange}
+      />
+    </div>
+    <Button
+      divCls='pt-1 p-4'
+      btnCls='btn-primary'
+    >Me Connecter</Button>
+  </form>
+
 }
+
 
 export default Login
--- assets/react/controllers/main_admin.jsx
+++ assets/react/controllers/main_admin.jsx
@@ -1,24 +1,12 @@
 import React from "react"
-import Navbar from "../components/admin/Navbar";
 import { BrowserRouter as Router } from "react-router-dom";
 import Layout from "../components/admin/Layout";
-import Content from "../components/admin/Content";
 
-import { Provider } from "react-redux";
-import store from '../components/admin/store'
 
 function MainAdmin() {
-  return <Provider store={store}>
-      <Router>
-      <Layout>
-        <Navbar/>
-        <Content>
-          <h1>Test</h1>
-
-        </Content>
-      </Layout>
+  return <Router>
+      <Layout/>
     </Router>
-  </Provider>
 }
 
 export default MainAdmin
