e9674e53cfe1b038b0f983b5671478d30665fcf1
alexis fritsch <arckanics.reiko@gmail.com>
2023-02-14 10:33:05 +0100

    [FRONT] group general func in 'app.js'

--- assets/react/components/Functions/app.js
+++ assets/react/components/Functions/app.js
@@ -1,6 +1,20 @@
 
-const getBaseURL = (ext = "") => {
+const setBaseUrl = (ext = "") => {
   return `${window.location.origin}/${ext}`
 }
 
-export { getBaseURL };
+const uppercase = (txt) => {
+  return txt.charAt(0).toUpperCase() + txt.slice(1)
+}
+
+const endOfPath = (path) => {
+  return path.slice(path.lastIndexOf('\/'))
+}
+
+const updateTitle = (n) => {
+  n = n.replace(/\//g, "")
+  let title = document.querySelector('title')
+  title.innerText = `Mini-CMS -> ${(n.length > 0 ? uppercase(n) : "Settings")}`
+}
+
+export { setBaseUrl, uppercase, updateTitle, endOfPath };
