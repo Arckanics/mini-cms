a67689e3c4fe70f64ae0b534549cd558d457469e
alexis fritsch <arckanics.reiko@gmail.com>
2023-02-23 10:17:21 +0100

    [FRONT] finish the select functionnality

--- assets/react/components/admin/ui/Selector.jsx
+++ assets/react/components/admin/ui/Selector.jsx
@@ -1,29 +1,46 @@
 import React, { useState } from 'react'
 import Expand from '../../../icon/icon-ui/Expand'
+import { uppercase } from '../../Functions/app'
 
-const Selector = ({ cls, list, active, iconCls, children }) => {
+const Selector = ({ cls, list, active, iconCls, children, action }) => {
   const [toggle, setToggle] = useState(false)
+  const [lastEvent, setLastEvent] = useState(null)
 
   const landing = list.find(i => i.id === active);
   const avaible = list.filter(i => i.id !== active)
 
   const toggleSelect = (e) => {
-    e.stopPropagation()
-    setToggle(!toggle);
+    e.stopPropagation();
+    let $el = e.target
+    while ($el.classList.contains('selector') == false) {
+      $el = $el.parentNode
+    }
+    if ((e.type === 'click' && lastEvent === 'click') || e.type !== 'click') {
+      setToggle(!toggle)
+      lastEvent === 'click' && toggle === true ? $el.blur() : null
+    } 
+    setLastEvent(e.type);
   }
 
+  const changeValue = (e,v) => {
+    toggleSelect(e);
+    action(v);
+  }
+
+
+
   return (
-    <div className={cls} onFocus={toggleSelect} onBlur={toggleSelect} html-type="select" tabIndex={-1}>
+    <div className={cls + ' selector'} onFocus={toggleSelect} onBlur={toggleSelect} tabIndex={-1}>
       { children }
-      <div className='opt active' value={landing.id}>
-        <div className='txt'>{landing.title}</div><Expand cls={iconCls}/>
+      <div className='opt active' value={landing.id} onClick={toggle ? toggleSelect : null}>
+        <div className='txt'>{uppercase(landing.title)}</div><Expand cls={iconCls}/>
       </div>
       {
         toggle ? <div className='opt-wrapper'>
           {
-            avaible.map((i,k) => 
-              <div key={k} className='opt' value={i.id}>
-                <div className='txt'>{i.title}</div>
+            avaible.map(({id,title},k) => 
+              <div key={k} className='opt' value={id} onClick={(e) => changeValue(e,id)} >
+                <div className='txt'>{uppercase(title)}</div>
               </div>
             )
           }
