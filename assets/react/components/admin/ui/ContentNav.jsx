3c8fde2225b91671a3dd8a193c782df8fb7fa312
alexis fritsch <arckanics.reiko@gmail.com>
2023-02-28 19:05:53 +0100

    [FRONT] editing 'Pages' navigation

--- assets/react/components/admin/ui/ContentNav.jsx
+++ assets/react/components/admin/ui/ContentNav.jsx
@@ -2,15 +2,36 @@ import React from 'react'
 import { capitalize } from '../../Functions/app'
 
 const ContentNav = ({ header, data }) => {
+
+  const setView = (value, set = 'value') => {
+    switch (set) {
+      case 'bool':
+      case 'boolean':
+        return value ? 'true' : 'false';
+      case 'value':
+      case 'number':
+      default:
+        return value;
+    }
+  }
+
   return (
     <section className='content-nav'>
       <div className='content-nav-header'>
         {
-          header.map((h,k) => <div key={k} className={`header-field colsize-${h.colSize}`}>{capitalize(h.name)}</div>)
+          header.map((h, k) => <div key={k} className={`header-field colsize-${h.colSize}`}>{capitalize(h.name)}</div>)
         }
+        <div className={`header-field colsize-1`}>{capitalize('actions')}</div>
       </div>
-      <div className='content-inner flex flex-col gap-3 py-3 justify-between overflow-auto overflow-x-hidden'>
-        
+      <div className='content-inner flex flex-col overflow-auto overflow-x-hidden'>
+        {
+          data && data.map((field, k) => <div key={k} className='content-row'>
+            {
+              header.map(({ tag, draw, colSize }, k) => <div key={k} className={`row-field colsize-${colSize}`}>{setView(field[tag],draw)}</div>)
+            }
+            <div className={`row-field colsize-1`}>{capitalize('actions')}</div>
+          </div>)
+        }
       </div>
     </section>
   )
