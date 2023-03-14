8a9e3af5bff8964a1aebd51b456a668850af7dae
alexis fritsch <arckanics.reiko@gmail.com>
2023-03-14 17:17:39 +0100

    [FRONT] Styling number input

--- assets/react/components/admin/ui/NumberInput.jsx
+++ assets/react/components/admin/ui/NumberInput.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const NumberInput = ({ cls, inpCls, change, value }) => {
   return (
-    <div className={cls + ' input-number-wrap flex flex-nowrap'}>
-      <input type='number' className={inpCls + ' colsize-8'} onChange={change} value={value} /><div className='flex flex-col arrow-btn-group colsize-2' ><span className='arrow-btn' target="up">+</span><span className='arrow-btn' target="down">-</span></div>
+    <div className={cls + ' input-number-wrap flex justify-items-stretch flex-nowrap relative'}>
+      <span className='arrow-btn' target="down">-</span><input type='number' className={inpCls + ' colsize-10'} onChange={change} value={value + 1} /><span className='arrow-btn' target="up">+</span>
     </div>
   )
 }
