0e893eaa44685e9883fd022627dba541cb262edd
alexis fritsch <arckanics.reiko@gmail.com>
2023-03-17 11:29:13 +0100

    [FRONT] setting up boolean filter & switch styles

--- assets/react/components/admin/ui/SwitchInput.jsx
+++ assets/react/components/admin/ui/SwitchInput.jsx
@@ -2,10 +2,10 @@
 
 import React from 'react'
 
-const SwitchInput = ({ cls = "", value }) => {
+const SwitchInput = ({ cls = "", value, change }) => {
   
   return (
-    <div className={'input-switch' + (value ? ' true': ' false') + ` ${cls}`} >
+    <div className={'input-switch' + (value ? ' true': ' false') + ` ${cls}`} onClick={(e) => change(e)}>
       <div className='input-switch-button'></div>
     </div>
   )
