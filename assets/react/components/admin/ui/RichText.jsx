c1167f1592a4322af4d691838d270ac707634f32
alexis fritsch <arckanics.reiko@gmail.com>
2023-03-24 18:09:06 +0100

    [FRONT] start integrating draftjs

--- assets/react/components/admin/ui/RichText.jsx
+++ assets/react/components/admin/ui/RichText.jsx
@@ -1,28 +1,55 @@
-import React, { useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import {ContentState, EditorState, handleKeyCommand} from 'draft-js';
 import { Editor } from "react-draft-wysiwyg";
 
-const RichText = ({ data }) => {
+const RichText = ({ data, change }) => {
   const [editorState, setEditorState] = useState(
-    () => EditorState.createEmpty(),
+    () => EditorState.createEmpty()
   );
+
+  useEffect(() => {
+  })
 
   const editor = useRef(null);
   const focusEditor = () => editor.current.focus();
 
+  const updateContent = (e) => {
+    
+    setEditorState(e)
+  }
+
 
   return (
-    <div className="text-editor w-full p-4"
-      style={{ cursor: "text" }}
+    <div className="text-editor w-full"
       onClick={focusEditor}
     >
+      <h1 className="editor-title">Contenu :</h1>
       <Editor
         ref={editor}
         editorState={editorState}
-        onEditorStateChange={setEditorState}
+        onEditorStateChange={updateContent}
         wrapperClassName="wrapper-class"
         editorClassName="editor-class"
         toolbarClassName="toolbar-class"
+        toolbar={{
+          options: [
+            'inline',
+            'fontSize',
+            'fontFamily',
+            'textAlign',
+            'list',
+            'link',
+            'image',
+            'history'
+          ],
+          textAlign: {
+            inDropdown: false,
+            className: undefined,
+            component: undefined,
+            dropdownClassName: undefined,
+            options: ['left', 'center', 'right', 'justify'],
+          }
+        }}
       />
     </div>
   )
