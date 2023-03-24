6477bb4d023c07466c06c15f32ddee17a470999e
alexis fritsch <arckanics.reiko@gmail.com>
2023-03-24 15:50:18 +0100

    [FRONT] installing draftjs

--- assets/react/components/admin/ui/RichText.jsx
+++ assets/react/components/admin/ui/RichText.jsx
@@ -1,2 +1,31 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React, { useRef, useState } from "react";
+import {ContentState, EditorState, handleKeyCommand} from 'draft-js';
+import { Editor } from "react-draft-wysiwyg";
+
+const RichText = ({ data }) => {
+  const [editorState, setEditorState] = useState(
+    () => EditorState.createEmpty(),
+  );
+
+  const editor = useRef(null);
+  const focusEditor = () => editor.current.focus();
+
+
+  return (
+    <div className="text-editor w-full p-4"
+      style={{ cursor: "text" }}
+      onClick={focusEditor}
+    >
+      <Editor
+        ref={editor}
+        editorState={editorState}
+        onEditorStateChange={setEditorState}
+        wrapperClassName="wrapper-class"
+        editorClassName="editor-class"
+        toolbarClassName="toolbar-class"
+      />
+    </div>
+  )
+}
+
+export default RichText
