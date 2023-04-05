af784f4f141b3bf4dd45f7e6d4073e49815220c1
alexis fritsch <arckanics.reiko@gmail.com>
2023-04-05 18:06:31 +0200

    [FRONT] images input modal (lexical)

---
+++ assets/react/components/admin/ui/RichText/Actions/items/imgModal.jsx
@@ -0,0 +1,10 @@
+import React, {useState} from 'react'
+
+const imgModal = () => {
+  const [inputFile, isInputFile] = useState(false)
+  return (
+    <div>imgModal</div>
+  )
+}
+
+export default imgModal
