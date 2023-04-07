cb8236e103dd4b6b0bb2f0e1ababb6775a69785a
alexis fritsch <arckanics.reiko@gmail.com>
2023-04-07 11:36:54 +0200

    [FRONT] imageModal online func added (lexical)

--- assets/react/components/admin/ui/RichText/Actions/InsertImg.jsx
+++ assets/react/components/admin/ui/RichText/Actions/InsertImg.jsx
@@ -5,38 +5,77 @@ import { INSERT_IMAGE_COMMAND } from '../plugins/ImagePlugin'
 import ImgModal from './items/imgModal'
 import { $getNodeByKey } from 'lexical'
 
-const InsertImg = () => {
-  const [modal, setModal] = useState(false)
+const InsertImg = ({nodeKey, open}) => {
+  const [modal, setModal] = useState(open || false)
   const [editor] = useLexicalComposerContext()
   const [imgProp, setImgProp] = useState({})
+  const [img, setImg] = useState(nodeKey || null)
 
   const openModal = (key) => {
-    editor.getEditorState().read(() => {
-      const el = $getNodeByKey(key)
-      setImgProp({src:el.__src,atEnd:el.__atEnd})
-      setModal(true)
-      console.log(imgProp);
+    console.log(key);
+    if (key) {
+      editor.getEditorState().read(() => {
+        const el = $getNodeByKey(key)
+        setImgProp({
+          src: {
+            isFile: false,
+            url: el.__src,
+          },
+          atEnd: el.__atEnd,
+        })
+        setImg(key)
+      })
+    } else {
+      setImgProp({
+        src: {
+          isFile: false,
+          url: "",
+        },
+        atEnd: false,
+      })
+    }
+    
+    setModal(true)
+  }
+
+  const editImg = ({name,value}) => {
+    if (img) {
+      editor.update(() => {
+        const el = $getNodeByKey(img)
+        const keys = el.getWritable()
+        keys[`__${name}`] = name === "src" ? value.url : value
+      })
+    }
+    setImgProp({
+      ...imgProp,
+      [name] : value
     })
-    
-    
   }
 
   const closeModal = () => {
     setModal(false)
+    setImg(null)
   }
 
   const openInsertImg = () => {
+    openModal(null)
+  }
+
+  const createImg = () => {
+    const { src, atEnd } = imgProp
     editor.dispatchCommand(INSERT_IMAGE_COMMAND, {
-      src: "https://picsum.photos/200",
-      alt: "lorem picsum",
+      src: src.url,
+      alt: null,
+      atEnd: atEnd,
       click: openModal
     })
+    setModal(false)
   }
   return (
     <div className="toolbar-item-group">
       <BtnFormatText command={openInsertImg}/>
       {
-        modal ? <ImgModal close={closeModal}/> : null
+        modal ? <ImgModal close={closeModal} update={editImg} create={img ? false : true} command={createImg} props={imgProp}/> : null
       }
     </div>
   )
--- assets/react/components/admin/ui/RichText/Actions/items/imgModal.jsx
+++ assets/react/components/admin/ui/RichText/Actions/items/imgModal.jsx
@@ -1,28 +1,34 @@
 import React, {useState} from 'react'
 import Close from "../../../../../../icon/icon-ui/Close"
-import {Checkbox, SwitchInput, TxtLabelInput} from "../../../"
+import {SwitchInput, TxtLabelInput} from "../../../"
 
 
 
-const ImgModal = ({close, command, props}) => {
-  const [inputFile, isInputFile] = useState(true)
+const ImgModal = ({close, update, props, create, command}) => {
+  const { atEnd, src } = props
   return (
     <div className='toolbar-modal'>
       <div className='modal-titlebar'>
         <div className='title-text' >Image :</div>
-        <button className="btn danger"><Close cls="icon" /></button>
+        <button className="btn danger" onClick={close}><Close cls="icon" /></button>
       </div>
       <div className='modal-content'>
           <div>
             <label className='p-2 underline' >Type d'image :</label>
             <div className='flex justify-between py-1 px-2'>
-              <label className={'font-bold' + (inputFile ? ' color-secondary' : "")}>En ligne</label>
-              <SwitchInput cls='secondary neutral' change={() => isInputFile(!inputFile)} value={!inputFile} />
-              <label className={'font-bold' + (!inputFile ? ' color-secondary' : "")}>Locale</label>
+              <label className={'font-bold' + (!src.isFile ? ' color-secondary' : "")}>En ligne</label>
+              <SwitchInput 
+                cls='secondary neutral' 
+                change={() => update(
+                  {name:'src', value:{...src, isFile: !src.isFile}}
+                  )
+                } 
+                value={src.isFile} />
+              <label className={'font-bold' + (src.isFile ? ' color-secondary' : "")}>Locale</label>
             </div>
           </div>
 
-          <div className='flex justify-between'>
+          <div className='flex justify-between p-1'>
             <TxtLabelInput 
               placeholder={"https://...."}
               label="Adresse :"
@@ -31,17 +37,26 @@ const ImgModal = ({close, command, props}) => {
               inputCls="input-txt secondary"
               type="text"
               id="urlInput"
-              value={null} 
+              value={props.src.url}
+              onChange={(e) => update({name:'src',value: {
+                ...src,
+                url: e.target.value,
+              }})}
             />
           </div>
           <div>
             <label className='p-2 underline' >Position :</label>
             <div className='flex justify-between py-1 px-2'>
-              <label className='font-bold'>Gauche</label>
-              <SwitchInput cls='secondary neutral' />
-              <label className='font-bold'>Droite</label>
+              <label className={'font-bold' + (!atEnd ? ' color-secondary' : "")}>Gauche</label>
+              <SwitchInput cls='secondary neutral' value={atEnd} change={() => update({name:'atEnd',value: !atEnd})}/>
+              <label className={'font-bold' + (atEnd ? ' color-secondary' : "")}>Droite</label>
             </div>
           </div>
+
+          { create 
+            ? <div className='py-1 px-2'><button className='btn secondary w-full rounded-lg p-1' onClick={command}>Insérer</button></div>
+            : null
+          }
       </div>
     </div>
   )
