e9f07408c16bbf2f091200ed73c35b0d35f05fa3
alexis fritsch <arckanics.reiko@gmail.com>
2023-04-17 15:59:44 +0200

    [FRONT] datepicker working function

--- assets/react/components/admin/ui/Inputs/DatePicker.jsx
+++ assets/react/components/admin/ui/Inputs/DatePicker.jsx
@@ -1,37 +1,60 @@
 import React, { useEffect, useState } from 'react'
-import Calendar from '../../../../icon/icon-ui/Calendar'
+import { Calendar, NavPrev, NavNext } from '../../../../icon/icon-ui/'
+
 
 const DatePicker = ({ change, value }) => {
+  value.setHours(0,0,0,0);
+  const currVal = {
+    day: value.getDay(),
+    date: value.getDate(),
+    month: value.getMonth(),
+    year: value.getFullYear(),
+  }
   const days = ["Lun","Mar","Mer","Jeu","Ven","Sam","Dim"]
-  const displayDate = new Date(value.getTime());
   const drawDate = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric', }
-  const [dateState, setDateState] = useState({
-    day: displayDate.getDay(),
-    date: displayDate.getDate(),
-    month: displayDate.getMonth(),
-    year: displayDate.getFullYear()
-  })
+  const [dateState, setDateState] = useState(new Date(value.getTime()))
+
+  const changeDate = (v) => {
+    const nDate = dateState
+    nDate.setDate(v)
+    change(nDate)
+  }
 
   const setDays = () => {
+    let count = 0;
     const outPut = []
-    const {day,date,month,year} = dateState
+    const displayDate = new Date(dateState.getTime())
     displayDate.setDate(1)
     for (let i = 1; i !== displayDate.getDay(); i++) {
-      outPut.push(<div></div>)
+      outPut.push(<div key={('dPick-'+count)} className='case empty'></div>)
+      count++
     }
-    displayDate.setMonth(month+1)
+    displayDate.setMonth(displayDate.getMonth()+1)
     displayDate.setDate(0)
     for (let i = 1; i <= displayDate.getDate(); i++) {
-      if (i == date && displayDate.getMonth() == month && displayDate.getFullYear() == year) {
-        outPut.push(<div className='case active'>{i}</div>)
+      if (i == currVal.date && currVal.month == displayDate.getMonth() && currVal.year == displayDate.getFullYear()) {
+        outPut.push(<div key={('dPick-'+count)} onClick={() => changeDate(i)} className='case active'>{i}</div>)
       } else {
-        outPut.push(<div className='case'>{i}</div>)
+        outPut.push(<div key={('dPick-'+count)} onClick={() => changeDate(i)} className='case'>{i}</div>)
       }
+      count++
     }
     return outPut;
   }
 
-  setDays()
+  const setMonth = (v) => {
+    let month = dateState.getMonth() + v
+    const nDate = new Date(dateState.getTime())
+    nDate.setMonth(month)
+    setDateState(nDate)
+  }
+
+  const setYear = (v) => {
+    let year = dateState.getFullYear() + v
+    const nDate = new Date(dateState.getTime())
+    nDate.setFullYear(year)
+    setDateState(nDate)
+  }
 
   return (
     <div className='input-calendar' tabIndex={-1}>
@@ -46,11 +69,23 @@ const DatePicker = ({ change, value }) => {
           </div>
         </div>
         <div className='day-month-select'>
-          <div className='calendar-year'>
-            {displayDate.toLocaleDateString("fr-FR", {year: "numeric"})}
-          </div>
           <div className='calendar-month'>
-            {displayDate.toLocaleDateString("fr-FR", {month: "long"})}
+            <div className='btn-nav primary' onClick={() => setMonth(-1)}>
+              <NavPrev cls="icon" />
+            </div>
+            <div>{dateState.toLocaleDateString("fr-FR", {month: "long"})}</div>
+            <div className='btn-nav primary' onClick={() => setMonth(1)}>
+              <NavNext cls="icon" />
+            </div>
+          </div>
+          <div className='calendar-year'>
+            <div className='btn-nav primary' onClick={() => setYear(-1)}>
+              <NavPrev cls="icon" />
+            </div>
+            <div>{dateState.toLocaleDateString("fr-FR", {year: "numeric"})}</div>
+            <div className='btn-nav primary' onClick={() => setYear(1)}>
+              <NavNext cls="icon" />
+            </div>
           </div>
         </div>
         <div className='day-names'>
