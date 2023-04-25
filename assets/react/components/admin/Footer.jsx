1c7064793dabc85885cf67cd105e77faa955c9c0
alexis fritsch <arckanics.reiko@gmail.com>
2023-04-25 16:38:31 +0200

    [FRONT] setting footer

--- assets/react/components/admin/Footer.jsx
+++ assets/react/components/admin/Footer.jsx
@@ -2,7 +2,19 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <footer id='admin-footer'>Footer</footer>
+    <footer id='admin-footer'>
+      <div className='link-group'>
+        <div className='link-prev'>Créateur : </div>
+        <a target='_blank' className='link' href='https://github.com/Arckanics'>
+          GitHub.
+        </a>
+      </div>
+      <div className='link-group'>
+        <a target='_blank' className='link' href='https://github.com/Arckanics/mini-cms'>
+          à propos
+        </a>
+      </div>
+    </footer>
   )
 }
 
