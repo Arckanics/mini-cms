1dbc78be9ddab23513d49bce5a5206f74dfa67ee
alexis fritsch <arckanics.reiko@gmail.com>
2023-05-09 10:08:27 +0200

    [BACK] initial site loading meta

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -2,17 +2,34 @@
 
 namespace App\Controller;
 
+use App\Entity\Pages;
+use App\Entity\Settings;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class IndexController extends AbstractController
 {
     #[Route('/{route}', name: 'app_index')]
-    public function index(String $route = ""): Response
+    public function index(Request $req, EntityManagerInterface $em, String $route = ""): Response
     {
+      $ajax = $req->headers->get('XmlHttpRequest');
+      if (!$ajax || $ajax === false) {
+        $settings = $em->getRepository(Settings::class)->find(1);
+        
+        return $this->render('/base.html.twig', [
+          "path" => $route,
+          "author" => $settings->getMetaAuthor(),
+          "desc" => $settings->getMetaDesc(),
+          "title" => $settings->getMetaSiteName(),
+          
+        ]);
+      }
       return $this->render('/base.html.twig', [
-        "url" => $route
+        "url" => $route,
+        "ajax" => !$ajax ? 'false': 'true'
       ]);
     }
 }
