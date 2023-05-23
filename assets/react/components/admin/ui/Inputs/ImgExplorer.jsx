966ec3264287f724ce1aca149996aa0c89ea354b
alexis fritsch <arckanics.reiko@gmail.com>
2023-05-23 12:03:54 +0200

    [BACK] logobrowser controller

--- assets/react/components/admin/ui/Inputs/ImgExplorer.jsx
+++ assets/react/components/admin/ui/Inputs/ImgExplorer.jsx
@@ -1,8 +1,21 @@
-import React, { useState } from 'react'
-
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+import { useSelector } from 'react-redux'
 const ImgExplorer = ({ label , labelCls, divCls, id, value }) => {
 
   const [target, setTarget] = useState(null)
+  const [list, setList] = useState(null)
+  const axiosSet = useSelector(state => state.ajax.axios);
+  const ajax = axios.create({ ...axiosSet });
+
+  useEffect(() => {
+    const path = '/logobrowser'
+    if (target) {
+      ajax.get(path).then((r) => {
+        console.log(r);
+      })
+    }
+  }, [target])
 
   return (
     <div className={divCls}>
@@ -11,7 +24,11 @@ const ImgExplorer = ({ label , labelCls, divCls, id, value }) => {
         ? <label htmlFor={(id ? id : null)} className={labelCls}>{label}</label>
         : null
       }
-      <div tabIndex={-1} id={(id ? id : null)} className='relative' onFocus={(e) => setTarget(e.target)}>
+      <div tabIndex={-1} id={(id ? id : null)} 
+        className='relative' 
+        onFocus={(e) => setTarget(e.target)}
+        onBlur={(e) => setTarget(null)}
+      >
         { 
           !value 
           ? <div className='btn secondary' >Choisir une image</div>
