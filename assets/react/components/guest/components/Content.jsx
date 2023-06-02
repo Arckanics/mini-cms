581a9b8a0da9afba47759c2c84b6b170fb0d808f
alexis fritsch <arckanics.reiko@gmail.com>
2023-06-02 15:33:48 +0200

    [FRONT] guest ui & ux styling

--- assets/react/components/guest/components/Content.jsx
+++ assets/react/components/guest/components/Content.jsx
@@ -1,15 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useSelector } from 'react-redux';
 import htmlReactParser from "html-react-parser"
 
 
 const Content = () => {
   const articles = useSelector(state => state.fetcher.articles)
-
   const parse = htmlReactParser
+  const [overflow, setOverflow] = useState(false)
+
+  const overflower = ref => {
+    const { scrollHeight } = ref
+    const { height } = ref.getBoundingClientRect()
+    setOverflow((scrollHeight > height))
+  }
 
   return (
-    <section id='content'>
+    <section id='content' 
+      className={overflow && 'scrollable'}
+      ref={node => (node ? overflower(node) : null)} >
+      <section className="wrapper">
       {
         articles
         && articles.map((a,k) => <article key={k} className='content-article'>
@@ -17,6 +26,7 @@ const Content = () => {
           { parse(a.Content) }
         </article>)
       }
+      </section>
 
     </section>
   )
