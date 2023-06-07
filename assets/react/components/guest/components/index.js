11645fa270c83289905eb31085bd872361a311b5
alexis fritsch <arckanics.reiko@gmail.com>
2023-06-07 15:54:01 +0200

    [FRONT] footer.jsx file created

--- assets/react/components/guest/components/index.js
+++ assets/react/components/guest/components/index.js
@@ -1,9 +1,11 @@
 import Content from "./Content";
+import Footer from "./Footer";
 import Header from "./Header";
 import Navbar from "./Navbar";
 
 export {
   Header,
   Navbar,
-  Content
+  Content,
+  Footer
 }
