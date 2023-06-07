f7f029c7a4a461684dc4944be4a66472df5ff18f
alexis fritsch <arckanics.reiko@gmail.com>
2023-06-07 15:05:11 +0200

    [FRONT] make 404 page

--- assets/react/components/guest/components/Content.jsx
+++ assets/react/components/guest/components/Content.jsx
@@ -1,11 +1,14 @@
 import React, { useEffect, useState } from 'react'
 import { useSelector } from 'react-redux';
 import htmlReactParser from "html-react-parser"
+import { useLocation } from 'react-router-dom';
 
 
 const Content = () => {
   const articles = useSelector(state => state.fetcher.articles)
+  const location = useLocation()
   const parse = htmlReactParser
+  const path = location.pathname
   const [ref, setRef] = useState(null)
   const [overflow, setOverflow] = useState(false)
 
@@ -18,18 +21,22 @@ const Content = () => {
 
   return (
     <section id='content' 
-      className={overflow ? 'scrollable' : null}
+      className={(!articles ? 'empty': '') + (overflow ? ' scrollable' : '')}
       ref={node => (node ? setRef(node) : null)} >
       <section className="wrapper">
       {
         articles
-        && articles.map((a,k) => <article key={k} className='article'>
+        ? articles.map((a,k) => <article key={k} className='article'>
           <h3 className='article-title'>{a.Title}</h3>
           <hr className='separator'/>
           <div className='article-content'>
             { parse(a.Content) }
           </div>
         </article>)
+        : <div className='message'>
+          <h1 className='code' >404</h1>
+          <p className='desc'>la page <span className='variable'>" {path} "</span> est introuvable...</p>
+        </div>
       }
       </section>
 
