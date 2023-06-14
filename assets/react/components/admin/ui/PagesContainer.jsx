c54b25d33c204685febe27ba4a0efd4155d11c9b
alexis fritsch <arckanics.reiko@gmail.com>
2023-06-14 08:04:58 +0200

    [FRONT] add one more PagesContainer setting

--- assets/react/components/admin/ui/PagesContainer.jsx
+++ assets/react/components/admin/ui/PagesContainer.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-const PagesContainer = ({children , title}) => {
+const PagesContainer = ({children , title, inDesign = true}) => {
   return (
     <div className="content-full">
-      <div className='title mb-8'>{title}</div>
+      <div className={'title mb-8' + (!inDesign ? ' out-design' : '')}>{title}</div>
       { children }
     </div>
   )
