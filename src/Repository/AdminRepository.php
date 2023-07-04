a048609c48277dd2551c27d39d71734b4088228e
alexis fritsch <arckanics.reiko@gmail.com>
2023-07-04 15:13:07 +0200

    [FULL] prepare response status for ResetPass

--- src/Repository/AdminRepository.php
+++ src/Repository/AdminRepository.php
@@ -19,65 +19,65 @@ use Symfony\Component\Security\Core\User\PasswordUpgraderInterface;
  */
 class AdminRepository extends ServiceEntityRepository implements PasswordUpgraderInterface
 {
-    public function __construct(ManagerRegistry $registry)
-    {
-        parent::__construct($registry, Admin::class);
+  public function __construct(ManagerRegistry $registry)
+  {
+    parent::__construct($registry, Admin::class);
+  }
+
+  public function save(Admin $entity, bool $flush = false): void
+  {
+    $this->getEntityManager()->persist($entity);
+
+    if ($flush) {
+      $this->getEntityManager()->flush();
+    }
+  }
+
+  public function remove(Admin $entity, bool $flush = false): void
+  {
+    $this->getEntityManager()->remove($entity);
+
+    if ($flush) {
+      $this->getEntityManager()->flush();
+    }
+  }
+
+  /**
+   * Used to upgrade (rehash) the user's password automatically over time.
+   */
+  public function upgradePassword(PasswordAuthenticatedUserInterface $user, string $newHashedPassword): void
+  {
+    if (!$user instanceof Admin) {
+      throw new UnsupportedUserException(sprintf('Instances of "%s" are not supported.', \get_class($user)));
     }
 
-    public function save(Admin $entity, bool $flush = false): void
-    {
-        $this->getEntityManager()->persist($entity);
+    $user->setPassword($newHashedPassword);
 
-        if ($flush) {
-            $this->getEntityManager()->flush();
-        }
-    }
+    $this->save($user, true);
+  }
 
-    public function remove(Admin $entity, bool $flush = false): void
-    {
-        $this->getEntityManager()->remove($entity);
+  //    /**
+  //     * @return Admin[] Returns an array of Admin objects
+  //     */
+  //    public function findByExampleField($value): array
+  //    {
+  //        return $this->createQueryBuilder('a')
+  //            ->andWhere('a.exampleField = :val')
+  //            ->setParameter('val', $value)
+  //            ->orderBy('a.id', 'ASC')
+  //            ->setMaxResults(10)
+  //            ->getQuery()
+  //            ->getResult()
+  //        ;
+  //    }
 
-        if ($flush) {
-            $this->getEntityManager()->flush();
-        }
-    }
-
-    /**
-     * Used to upgrade (rehash) the user's password automatically over time.
-     */
-    public function upgradePassword(PasswordAuthenticatedUserInterface $user, string $newHashedPassword): void
-    {
-        if (!$user instanceof Admin) {
-            throw new UnsupportedUserException(sprintf('Instances of "%s" are not supported.', \get_class($user)));
-        }
-
-        $user->setPassword($newHashedPassword);
-
-        $this->save($user, true);
-    }
-
-//    /**
-//     * @return Admin[] Returns an array of Admin objects
-//     */
-//    public function findByExampleField($value): array
-//    {
-//        return $this->createQueryBuilder('a')
-//            ->andWhere('a.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->orderBy('a.id', 'ASC')
-//            ->setMaxResults(10)
-//            ->getQuery()
-//            ->getResult()
-//        ;
-//    }
-
-//    public function findOneBySomeField($value): ?Admin
-//    {
-//        return $this->createQueryBuilder('a')
-//            ->andWhere('a.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->getQuery()
-//            ->getOneOrNullResult()
-//        ;
-//    }
+  //    public function findOneBySomeField($value): ?Admin
+  //    {
+  //        return $this->createQueryBuilder('a')
+  //            ->andWhere('a.exampleField = :val')
+  //            ->setParameter('val', $value)
+  //            ->getQuery()
+  //            ->getOneOrNullResult()
+  //        ;
+  //    }
 }
