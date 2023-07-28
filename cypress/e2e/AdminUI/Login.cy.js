e39932fb56641fd1862a679e614c7de7fe9a205d
Arckanics <arckanics.reiko@gmail.com>
2023-07-28 12:49:53 +0200

    [TEST] initial cypress install

---
+++ cypress/e2e/AdminUI/Login.cy.js
@@ -0,0 +1,11 @@
+/// <reference types="cypress" />
+
+describe('initiate', () => {
+  beforeEach(() => {
+    cy.visit('https://www.mini-cms.fr/mini-admin')
+  })
+
+  it("display the login page from Admin ui", () => {
+    
+  })
+})
